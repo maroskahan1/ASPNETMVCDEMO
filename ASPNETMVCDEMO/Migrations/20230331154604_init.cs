b74202e9817551fcd5ba3d70b7f37a139a37cee5
mkahan <maros.kahan@alanata.sk>
2023-03-31 17:53:29 +0200

    Added Job entity and init ef migration

---
+++ ASPNETMVCDEMO/Migrations/20230331154604_init.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASPNETMVCDEMO.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jobs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfLastStart = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Interval = table.Column<int>(type: "int", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jobs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jobs");
+        }
+    }
+}
---
+++ ASPNETMVCDEMO/Models/Domain/Job.cs
@@ -0,0 +1,12 @@
+﻿namespace ASPNETMVCDEMO.Models.Domain
+{
+    public class Job 
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfLastStart { get; set; }
+        public int Interval { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+    }
+}
